2629179b84bf0ae4d095b2232a9ae7fde652fd21
Unknown <dbbarret@indiana.edu>
2018-06-25 19:58:31 -0400

    fix: alt tag on img

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -57,6 +57,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.alt='photograph of ' + restaurant.name + ' restaurant'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
